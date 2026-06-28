d9245d6b14d3f9c2756e1d58537cd6903599ec5c
KaioRaul <kaioraul5@gmail.com>
2026-06-28 20:46:50 +0200

    feat: added variants to Parchment component

--- src/components/parchment/Parchment.tsx
+++ src/components/parchment/Parchment.tsx
@@ -1,7 +1,15 @@
 import css from "./Parchment.module.css";
 
-const Parchment = ({ children }: Readonly<{ children: React.ReactNode }>) => {
-  return <div className={css.parchment}>{children}</div>;
+interface ParchmentProps {
+  children: React.ReactNode;
+  ParchmentVariant?: string;
+}
+
+const Parchment = ({ children, ParchmentVariant = "01" }: ParchmentProps) => {
+  const variantKey = `parchment${ParchmentVariant}`;
+  const className = css[variantKey];
+
+  return <div className={className}>{children}</div>;
 };
 
 export default Parchment;
