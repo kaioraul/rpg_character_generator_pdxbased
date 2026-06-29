5e35ca632219c4f9ba3dfd8c972460f97c63ea83
KaioRaul <kaioraul5@gmail.com>
2026-06-29 21:02:37 +0200

    feat: started adding strategy to  base page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,16 @@ import css from "./page.module.css";
 export default function Home() {
   return (
     <main className={css.main}>
-      <div className={css.leftContainer}>Left container</div>
+      <div className={css.leftContainer}>
+        <div>House Coats of Arms</div>
+        <div>Portrait</div>
+        <div>Family Coats of Arms</div>
+        <input readOnly type="text" value="Name" />
+        <input readOnly type="text" value="Title" />
+        <input readOnly type="text" value="House" />
+        <div>Statistics</div>
+        <div>Traits</div>
+      </div>
       <div className={css.centerContainer}>Center container</div>
       <div className={css.rightContainer}>Right container</div>
     </main>
