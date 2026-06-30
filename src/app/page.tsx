1b2a31722fe093cc76a3ee86c2d473a69b5e35e4
KaioRaul <kaioraul5@gmail.com>
2026-06-30 17:35:53 +0200

    feat: added strategy left container on base page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,24 @@ export default function Home() {
   return (
     <main className={css.main}>
       <div className={css.leftContainer}>
-        <div>House Coats of Arms</div>
-        <div>Portrait</div>
-        <div>Family Coats of Arms</div>
+        <div className={css.leftContainerTop}>
+          <div className={css.houseCoA}>House Coats of Arms</div>
+          <div className={css.portrait}>Portrait</div>
+          <div className={css.familyCoA}>Family Coats of Arms</div>
+        </div>
         <input readOnly type="text" value="Name" />
         <input readOnly type="text" value="Title" />
         <input readOnly type="text" value="House" />
         <div>Statistics</div>
+      </div>
+      <div className={css.centerContainer}>
+        <div>OCEAN model</div>
         <div>Traits</div>
       </div>
-      <div className={css.centerContainer}>Center container</div>
-      <div className={css.rightContainer}>Right container</div>
+      <div className={css.rightContainer}>
+        <button>Save</button>
+        <button>Export to PDF</button>
+      </div>
     </main>
   );
 }
