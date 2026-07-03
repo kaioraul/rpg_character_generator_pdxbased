d2e35f3a5ac45a31ace38691b37a36f214bfe44f
KaioRaul <kaioraul5@gmail.com>
2026-07-03 15:08:59 +0200

    feat: added db

---
+++ src/db/cmd.sql
@@ -0,0 +1,11 @@
+-- To add a new column to the table:
+-- ALTER TABLE traits ADD COLUMN status TEXT;
+-- To change the data type of a column:
+--ALTER TABLE traits ALTER COLUMN status TYPE VARCHAR(255);
+SELECT * FROM traits;
+
+-- delete table
+--DROP TABLE traits;
+
+-- rename table
+--ALTER TABLE traits_new RENAME TO traits;
